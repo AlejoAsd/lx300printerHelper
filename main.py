c4dd922f0fcd8cdf99827361fc6264e34050a913
torresmateo <torresmateo@gmail.com>
2015-02-26 18:03:42 -0300

    listen on host 0.0.0.0

--- main.py
+++ main.py
@@ -88,4 +88,4 @@ def config():
         return render_template('config.html', dev=dev)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=52738)
+    app.run(debug=True, port=52738, host='0.0.0.0')
